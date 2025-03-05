86b56815f106fcdfd1e8d8c83686314976f36349
Ivan Herhi <ionherhi@gmail.com>
2025-03-05 18:14:24 +0100

    LastWork_ImNOW

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,32 @@
 const body = document.body;
 const allInputs = body.querySelectorAll('input')
 const arrayInputs = [...allInputs];
-const fielD body.querySelectorAll('.field');
+const field = body.querySelectorAll('.field');
 
 arrayInputs.forEach((item) => {
-  item.setAttribute('placeholder', item.getAttribute('name').toUpperCase());
+  const name = item.getAttribute('name')
+  item.setAttribute('placeholder', name[0].toUpperCase() + name.slice(1));
 });
+
+const arrayFromAtributs = arrayInputs.map((item) => {
+  return item.getAttribute('id');
+})
+
+
+  faielD.forEach((item) => {
+    const labelElement = document.createElement('label');
+    let curent;
+
+    for (let i = 0; i < arrayFromAtributs.length; i++) {
+      curent = arrayFromAtributs[i]
+      labelElement.textContent = arrayInputs[i].name;
+    };
+
+    labelElement.setAttribute('for', curent);
+    labelElement.setAttribute('class', 'field-label')
+
+    item.prepend(labelElement);
+  });
+
+  /* ss */
 
